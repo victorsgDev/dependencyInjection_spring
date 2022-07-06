9449b0c25cac0dbf6518e2d1be1eca1c273b83cb
Victor Sanchez Grande <victorsg.dev@gmail.com>
2022-07-06 13:38:36 +0200

    DI mediante Java config

--- src/main/java/com/example/victor/MainApp.java
+++ src/main/java/com/example/victor/MainApp.java
@@ -1,13 +1,15 @@
 package com.example.victor;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-       ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+       //ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+       ApplicationContext context= new AnnotationConfigApplicationContext(AppConfig.class);
 
        EmailClient emailClient = context.getBean("emailClient", EmailClient.class);
        emailClient.sendEmail("Este es mi primer email");
